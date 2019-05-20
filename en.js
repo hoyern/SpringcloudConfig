807cfa2065722e8510bef840d6486c7c373c2ea8
hoyern <35024626+hoyern@users.noreply.github.com>
2019-05-20 16:42:53 +0800

    add file

---
+++ en.js
@@ -0,0 +1,24 @@
+export const App = {
+    Home: 'Home',
+    Imports: 'Imports',
+    Guidelines: 'Guidelines',
+    Reports: 'Reports',
+    Manage: 'Manage',
+    ManageStations: 'Manage Stations',
+    ManageGuidelines: 'Manage Guidelines',
+    System: 'System',
+    Administration: 'Administration',
+    SearchField: 'Jump to Station',
+    Logout: 'Logout',
+    Language: 'Language',
+};
+
+export const StationSideMenu = {
+    ContinuousDataStations: 'Continuous Data Stations',
+    DiscreteSampleStations: 'Discrete Sample Stations'
+};
+
+export const GuidelinesDetailForm = {
+    updateSuccessMsg: 'Guideline {msg} updated successfully',
+    addSuccessMsg: 'Guideline {msg} added successfully'
+};
---
+++ zh.js
@@ -0,0 +1,24 @@
+export const App = {
+    Home: '主页',
+    Imports: '导入',
+    Guidelines: '标准',
+    Reports: '报告',
+    Manage: '管理',
+    ManageStations: '管理监测站',
+    ManageGuidelines: '管理标准',
+    System: '系统管理',
+    Administration: '',
+    SearchField: '跳转至监测站',
+    Logout: '登出',
+    Language: '选择语言',
+};
+
+export const StationSideMenu = {
+    ContinuousDataStations: '连续数据监测站',
+    DiscreteSampleStations: '离散样品检测站'
+};
+
+export const GuidelinesDetailForm = {
+    updateSuccessMsg: '标准 {msg} 更新成功',
+    addSuccessMsg: '标准 {msg} 添加成功'
+};
